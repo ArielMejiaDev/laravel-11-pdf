492da4f4b3e0fc8d8a2a65385a17b95396b926b2
ArielMejiaDev <arielmejiadev@gmail.com>
2024-07-29 12:21:35 -0600

    set a pdf example

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Spatie\Browsershot\Browsershot;
 
-Route::get('/', function () {
+Route::get('/', static function () {
     return view('welcome');
+});
+
+Route::get('/pdf', static function () {
+    $html = view('pdf.example')->render();
+
+    $pdf = Browsershot::html($html)
+        ->pdf();
+
+    return response($pdf)
+        ->header('Content-Type', 'application/pdf')
+        ->header('Content-Disposition', 'inline; filename="document.pdf"');
 });
